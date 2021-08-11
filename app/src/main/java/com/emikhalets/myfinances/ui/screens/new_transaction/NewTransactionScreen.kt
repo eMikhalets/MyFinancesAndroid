b0cec7a1344ce0ad08283972c69fc01c93145065
Mikhalets Evgeny <evg.mikhalets@gmail.com>
2021-08-11 23:47:06 +0300

    refactor: new transactions screen

--- app/src/main/java/com/emikhalets/myfinances/ui/screens/new_transaction/NewTransactionScreen.kt
+++ app/src/main/java/com/emikhalets/myfinances/ui/screens/new_transaction/NewTransactionScreen.kt
@@ -1,15 +1,17 @@
 package com.emikhalets.myfinances.ui.screens.new_transaction
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -17,6 +19,7 @@ import androidx.navigation.NavHostController
 import com.emikhalets.myfinances.R
 import com.emikhalets.myfinances.data.entity.Category
 import com.emikhalets.myfinances.ui.screens.dialogs.ChooseCategoryDialog
+import com.emikhalets.myfinances.utils.enums.KeyboardKey
 import com.emikhalets.myfinances.utils.enums.TransactionType
 
 @Composable
@@ -26,6 +29,7 @@ fun NewTransactionScreen(
     viewModel: NewTransactionVM = hiltViewModel()
 ) {
     var note by remember { mutableStateOf("") }
+    var value by remember { mutableStateOf("") }
     var selectedCategory by remember { mutableStateOf<Category?>(null) }
 
     when (val state = viewModel.state) {
@@ -36,7 +40,9 @@ fun NewTransactionScreen(
                 selectedCategory = selectedCategory,
                 onCategoryChange = { selectedCategory = it },
                 note = note,
-                onNoteChange = { note = it }
+                value = value,
+                onNoteChange = { note = it },
+                onValueChange = { value = it }
             )
         }
         is NewTransactionState.Error -> {
@@ -51,7 +57,9 @@ fun NewTransactionScreen(
                 selectedCategory = selectedCategory,
                 onCategoryChange = { selectedCategory = it },
                 note = note,
-                onNoteChange = { note = it }
+                value = value,
+                onNoteChange = { note = it },
+                onValueChange = { value = it }
             )
         }
     }
@@ -64,7 +72,9 @@ fun NewTransactionScreen(
     selectedCategory: Category?,
     onCategoryChange: (Category) -> Unit,
     note: String,
-    onNoteChange: (String) -> Unit
+    value: String,
+    onNoteChange: (String) -> Unit,
+    onValueChange: (String) -> Unit
 ) {
     var addCategoryDialog by remember { mutableStateOf(false) }
     val label = when (transactionType) {
@@ -73,26 +83,55 @@ fun NewTransactionScreen(
     }
 
     Column {
-        Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = label,
             textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        CategoryButton(
-            name = selectedCategory?.name ?: stringResource(R.string.no_categories),
-            icon = painterResource(selectedCategory?.icon ?: R.drawable.ic_coins),
+            color = MaterialTheme.colors.onPrimary,
             modifier = Modifier
-                .padding(16.dp)
+                .background(color = MaterialTheme.colors.primary)
                 .fillMaxWidth()
-                .clickable { addCategoryDialog = true }
+                .padding(top = 8.dp, bottom = 8.dp)
         )
-        Spacer(modifier = Modifier.height(16.dp))
+        Row(Modifier.fillMaxWidth()) {
+            NewTransactionButton(
+                label = stringResource(R.string.wallet),
+                name = selectedCategory?.name ?: stringResource(R.string.no_categories),
+                icon = R.drawable.ic_wallet,
+                color = MaterialTheme.colors.primary.copy(alpha = 0.8f),
+                modifier = Modifier
+                    .weight(1f)
+                    .clickable {}
+            )
+            NewTransactionButton(
+                label = stringResource(R.string.category),
+                name = selectedCategory?.name ?: stringResource(R.string.no_categories),
+                icon = R.drawable.ic_coins,
+                color = MaterialTheme.colors.primary.copy(alpha = 0.5f),
+                modifier = Modifier
+                    .weight(1f)
+                    .clickable { addCategoryDialog = true }
+            )
+        }
         TextField(
             value = note,
             onValueChange = { onNoteChange(it) },
             label = { Text(stringResource(R.string.note)) },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = MaterialTheme.colors.surface
+            ),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
+        TextField(
+            value = value,
+            onValueChange = { onValueChange(it) },
+            label = { Text(stringResource(R.string.value)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = MaterialTheme.colors.surface
+            ),
             modifier = Modifier
                 .padding(16.dp)
                 .fillMaxWidth()
@@ -108,19 +147,117 @@ fun NewTransactionScreen(
 }
 
 @Composable
-fun CategoryButton(
+fun NewTransactionButton(
+    label: String,
     name: String,
-    icon: Painter,
-    modifier: Modifier = Modifier,
+    icon: Int,
+    color: Color,
+    modifier: Modifier = Modifier
 ) {
-    Row(modifier = modifier.padding(8.dp)) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .background(color = color)
+            .padding(8.dp)
+    ) {
         Icon(
-            painter = icon,
-            contentDescription = ""
+            painter = painterResource(icon),
+            contentDescription = "",
+            modifier = Modifier.size(50.dp)
         )
-        Spacer(modifier = Modifier.width(16.dp))
-        Text(
-            text = name
-        )
+        Spacer(Modifier.width(16.dp))
+        Column {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.onPrimary
+            )
+            Text(
+                text = name,
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.onPrimary
+            )
+        }
+    }
+}
+
+@Composable
+fun NewTransactionKeyboard(
+    onClickKey: (Int) -> Unit
+) {
+    Column(Modifier.padding(4.dp)) {
+        Row {
+            KeyboardButton(
+                value = KeyboardKey.Num1,
+                onClickKey = {}
+            )
+            KeyboardButton(
+                value = KeyboardKey.Num2,
+                onClickKey = {}
+            )
+            KeyboardButton(
+                value = KeyboardKey.Num3,
+                onClickKey = {}
+            )
+        }
+        Row {
+            KeyboardButton(
+                value = KeyboardKey.Num4,
+                onClickKey = {}
+            )
+            KeyboardButton(
+                value = KeyboardKey.Num5,
+                onClickKey = {}
+            )
+            KeyboardButton(
+                value = KeyboardKey.Num6,
+                onClickKey = {}
+            )
+        }
+        Row {
+            KeyboardButton(
+                value = KeyboardKey.Num7,
+                onClickKey = {}
+            )
+            KeyboardButton(
+                value = KeyboardKey.Num8,
+                onClickKey = {}
+            )
+            KeyboardButton(
+                value = KeyboardKey.Num9,
+                onClickKey = {}
+            )
+        }
+        Row {
+            KeyboardButton(
+                value = KeyboardKey.Num0,
+                onClickKey = {}
+            )
+            KeyboardButton(
+                value = KeyboardKey.Dot,
+                onClickKey = {}
+            )
+            KeyboardButton(
+                value = KeyboardKey.Delete,
+                onClickKey = {}
+            )
+        }
+    }
+}
+
+@Composable
+fun KeyboardButton(
+    value: KeyboardKey,
+    onClickKey: (KeyboardKey) -> Unit
+) {
+    Button(
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.primary,
+            contentColor = MaterialTheme.colors.onPrimary
+        ),
+        modifier = Modifier.padding(4.dp),
+        onClick = { onClickKey(value) }
+    ) {
+        Text(value.value)
     }
 }
--- app/src/main/java/com/emikhalets/myfinances/ui/theme/Type.kt
+++ app/src/main/java/com/emikhalets/myfinances/ui/theme/Type.kt
@@ -2,27 +2,22 @@ package com.emikhalets.myfinances.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.emikhalets.myfinances.R
 
-// Set of Material typography styles to start with
+val Montserrat = FontFamily(
+    Font(R.font.montserrat),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
+    defaultFontFamily = Montserrat,
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )

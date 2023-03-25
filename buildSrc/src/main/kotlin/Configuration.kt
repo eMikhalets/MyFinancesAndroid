5081ae65a99c631717a9dd048e9d8b9e9bc144f9
Mikhalets Evgeny <evg.mikhalets@gmail.com>
2023-03-25 21:52:11 +0600

    add buildSrc module

---
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -0,0 +1,9 @@
+object Configuration {
+    const val namespace = "com.emikhalets.myfinances"
+    const val applicationId = "com.emikhalets.myfinances"
+    const val compileSdk = 33
+    const val minSdk = 26
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}

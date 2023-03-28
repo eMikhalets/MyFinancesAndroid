4a92cfabac9ea3192be60e32519b32368faaf065
Mikhalets Evgeny <evg.mikhalets@gmail.com>
2023-03-28 22:04:11 +0600

    fixes and clean code in core

--- core/src/main/java/com/emikhalets/core/Extensions.kt
+++ core/src/main/java/com/emikhalets/core/Extensions.kt
@@ -1,40 +1,13 @@
 package com.emikhalets.core
 
-import android.content.Context
 import android.content.res.Resources
-import android.widget.Toast
 import androidx.annotation.StringRes
 import java.text.SimpleDateFormat
 import java.util.*
 
 fun getString(@StringRes res: Int): String = Resources.getSystem().getString(res)
 
-// ========================================
-// Global constants
-
-const val DEFAULT_ERROR = "null"
-
-// ========================================
-// Primitive conversion
-
-fun Double?.formatMoney(withCurrency: Boolean = false): String {
-    val valueString = this.toString()
-    return try {
-        val valueArray = valueString.split(".")
-        val remain = valueArray[1]
-        val newRemain = if (remain.length == 1) "${remain}0" else remain
-        return buildString {
-            append(valueArray[0])
-            append(".")
-            append(newRemain)
-            if (withCurrency) append(" ₽")
-        }
-    } catch (ex: Exception) {
-        if (withCurrency) "0.00  ₽" else "0.00"
-    }
-}
-
-inline fun String.safeToDouble(crossinline onError: () -> Unit = {}): Double {
+inline fun String.toDoubleSafe(crossinline onError: () -> Unit = {}): Double {
     if (this.isEmpty() || this.isBlank()) return 0.0
     return try {
         val converted: Double = this.toDouble()
@@ -46,66 +19,14 @@ inline fun String.safeToDouble(crossinline onError: () -> Unit = {}): Double {
     }
 }
 
-fun Double.toMoney(): String {
-    return (this * 100).toInt().toString()
-}
-
-fun String.toMoney(): String {
-    return try {
-        val converted: Double = this.toDouble()
-        (converted * 100).toInt().toString()
-    } catch (ex: Exception) {
-        ex.printStackTrace()
-        ""
-    }
-}
-
-// ========================================
-// Date formatting
-
 fun Long?.toDate(): String {
     this ?: return "no date"
     val formatter = SimpleDateFormat("d MMM yyyy", Locale.getDefault())
     return formatter.format(Date(this))
 }
 
-fun Long?.toLabelDate(): String {
-    if (this == null) return "no date"
-    val date = Date(this)
-    val formatter = SimpleDateFormat("d MMMM yyyy", Locale.getDefault())
-    return formatter.format(date)
+fun Long?.formatDate(pattern: String = "dd MMM yyyy"): String? {
+    this ?: return null
+    val formatter = SimpleDateFormat(pattern, Locale.getDefault())
+    return formatter.format(Date(this))
 }
-
-// ========================================
-// Toasts and snackBars
-
-fun toast(context: Context, @StringRes res: Int) {
-    Toast.makeText(context, res, Toast.LENGTH_SHORT).show()
-}
-
-//fun toast(context: Context, message: String) {
-//    if (message.isEmpty()) return
-//    Toast.makeText(context, message.errorOrDefault(context), Toast.LENGTH_SHORT).show()
-//}
-
-//private fun String.errorOrDefault(context: Context): String {
-//    return if (this == DEFAULT_ERROR) context.getString(R.string.default_error) else this
-//}
-
-// ========================================
-// Compose
-
-//@Composable
-//fun String.formatValue(): String {
-//    val thousandsString = this
-//    val resourceString = stringResource(R.string.app_money_value, thousandsString)
-//    return this
-//}
-//
-//fun String.appKeyboardInput(oldValue: String): String {
-//    return if (this == "X") {
-//        ""
-//    } else {
-//        oldValue + this
-//    }
-//}
--- core/src/main/java/com/emikhalets/core/Prefs.kt
+++ core/src/main/java/com/emikhalets/core/Prefs.kt
@@ -9,12 +9,17 @@ class Prefs @Inject constructor(@ApplicationContext context: Context) {
 
     private var sp: SharedPreferences = context.getSharedPreferences(NAME, Context.MODE_PRIVATE)
 
+    var isAppInitialised: Boolean
+        get() = sp.getBoolean(APP_INITIALISED, false)
+        set(value) = sp.edit().putBoolean(APP_INITIALISED, value).apply()
+
     var currentWalletId: Long
-        get() = sp.getLong(CURRENT_WALLET_ID, WALLET_ID)
+        get() = sp.getLong(CURRENT_WALLET_ID, DEFAULT_WALLET_ID)
         set(value) = sp.edit().putLong(CURRENT_WALLET_ID, value).apply()
 
     companion object {
         private const val NAME = "MyFinances_Preferences"
+        private const val APP_INITIALISED = "APP_INITIALISED"
         private const val CURRENT_WALLET_ID = "CURRENT_WALLET_ID"
     }
 }
